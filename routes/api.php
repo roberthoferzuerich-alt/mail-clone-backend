7f8009d3d6d82642523727b70ace55f6ffb41e9d
RobHoferZuerich <robert.hofer.zuerich@bluewin.ch>
2026-09-23 09:09:35 +0200

    Fix: Replace Client::purge with Client::make for dynamic IMAP

--- routes/api.php
+++ routes/api.php
@@ -231,19 +231,15 @@ Route::put('/mail-accounts/{id}', function (Request $request, $id) {
         }
 
         try {
-            config([
-                'imap.accounts.default.host' => $account->imap_host,
-                'imap.accounts.default.port' => $account->imap_port,
-                'imap.accounts.default.encryption' => $account->imap_port == 993 ? 'ssl' : 'tls',
-                'imap.accounts.default.validate_cert' => false,
-                'imap.accounts.default.username' => $account->email,
-                'imap.accounts.default.password' => $account->password,
-                'imap.accounts.default.protocol' => 'imap',
+            $client = \Webklex\IMAP\Facades\Client::make([
+                'host'          => $account->imap_host,
+                'port'          => $account->imap_port,
+                'encryption'    => $account->imap_port == 993 ? 'ssl' : 'tls',
+                'validate_cert' => false,
+                'username'      => $account->email,
+                'password'      => $account->password,
+                'protocol'      => 'imap'
             ]);
-
-            // Clear cache and connect
-            \Webklex\IMAP\Facades\Client::purge('default');
-            $client = \Webklex\IMAP\Facades\Client::account('default');
             $client->connect();
 
             $folder = $client->getFolder('INBOX');
